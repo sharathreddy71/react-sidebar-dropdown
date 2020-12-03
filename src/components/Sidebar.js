0e3a2e363ed79117b09dd4d573587018d8ba33b1
sharathreddy71 <57720044+sharathreddy71@users.noreply.github.com>
2020-12-03 14:43:40 +0100

    Update Sidebar.js

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
+import React, { useState } from 'react'
+import styled from 'styled-components'
 import { Link } from 'react-router-dom';
 import * as FaIcons from 'react-icons/fa';
 import * as AiIcons from 'react-icons/ai';
